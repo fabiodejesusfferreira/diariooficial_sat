d404e1f676b6be13855a7e33a7f4eeaa08dacafa
Fábio de Jesus <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-09-18 21:15:31 -0300

    feat: Adiciona novo diário via API

--- src/api/src/database/diarioDatabase.ts
+++ src/api/src/database/diarioDatabase.ts
@@ -1,9 +1,23 @@
 export const database = [
     {
-        id: 1,
-        date: 1757510818036,
-        tags: ["politica", "formulario", "documento"],
-        pdf: '',
-        size: ''
+        "id": 1,
+        "date": 1757510818036,
+        "tags": [
+            "politica",
+            "formulario",
+            "documento"
+        ],
+        "pdf": "",
+        "size": ""
+    },
+    {
+        "id": 2,
+        "date": 1757510999999,
+        "tags": [
+            "novo",
+            "item"
+        ],
+        "pdf": "",
+        "size": ""
     }
-]
+];
